43fd95876a128dd30934b937e191f6cde2063980
Yuri Nalin <yuri.nalin.rms@gmail.com>
2021-07-15 20:36:16 -0300

    fix: Code cleanup and new README

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -3,31 +3,6 @@ import { intervalToDuration, isSameMinute, isAfter, addMinutes } from 'date-fns'
 import settings from './config/settings.js';
 import messages from './config/messages.js';
 
-function computeClockOutTime_Old(timeList = []) {
-    if (timeList.length < 1 || timeList.length % 2 === 0)
-        return messages.WRONG_NUMBER_ENTRIES;
-
-    timeList = orderTimeList(timeList, true);
-
-    let totalDuration = settings.DAILY_WORK_TIME;
-    let tailIndex = timeList.length - 1;
-    while (tailIndex > 0) {
-        //totalDuration.subtract(moment.duration(timeList[tailIndex].diff(timeList[tailIndex - 1])));
-
-        if (totalDuration.minutes < -1 * (settings.MAX_EXTRA(settings).minutes))
-            return messages.EXCEEDS_MAX_ALLOWED;
-
-        timeList.pop();
-        timeList.pop();
-        tailIndex -= 2;
-    }
-
-    if (totalDuration < 0)
-        return '\nERROR: Extra hours: Not implemented.';
-
-    return timeList[0].subtract(totalDuration);
-}
-
 function computeClockOutTime(timeList = []) {
     if (timeList.length < 1 && timeList.length % 2 === 0)
         return messages.WRONG_NUMBER_ENTRIES;
@@ -65,13 +40,3 @@ function orderTimeList(timeList, reverse) {
 
     return reverse ? orderedList.reverse() : orderedList;
 }
-
-// function printDuration(duration = moment.duration(1, 's'), isDebug = false, asMinutes = false) {
-//     let timeToPrint = asMinutes ?
-//         `${duration}min` :
-//         `${duration.hours}:${duration.minutes}`;
-
-//     return isDebug ? console.log(timeToPrint) : timeToPrint;
-// }
-
-computeClockOutTime(settings.TIME_LIST);
